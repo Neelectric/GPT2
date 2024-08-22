d905edb02423ee731f1838c97988e8d7eff22426
Neelectric <Neel.R@web.de>
2024-08-22 22:22:33 +0200

    its getting spicy

--- train_gpt2.py
+++ train_gpt2.py
@@ -1,10 +1,62 @@
+import math
 from dataclasses import dataclass
 import torch
 import torch.nn as nn
 from torch.nn import functional
 
-
 # -------------------------------------------- #
+
+class CausalSelfAttention(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        assert config.n_embd % config.n_head == 0
+        # key, query and value projections for all heads, but batched!
+        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
+        # output projection
+        self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+        # regularization
+        self.n_head = config.n_head
+        self.n_embd = config.n_embd
+        #technically a mask not a bias but follows HF/OAI naming
+        self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))).view(1, 1, config.block_size, config.block_size)
+
+    def forward(self, x):
+        B, T, C = x.size() # batch size, sequence length, embedding dimensionality (n_embd)
+        # calculate q, k and v for all heads in batch and move head forward to be the batch
+        # nh is num_heads, hs is head_size, C (number of channels) is nh * ns
+        # so for GPT-2 (124M), n_head=12, hs=64, nh*hs=C=768 channels in transformer
+        qkv = self.c_attn(x)
+        q, k, v = qkv.split(self.n_embd, dim=2)
+
+
+class MLP(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd)
+        self.gelu = nn.GELU(approximate='tanh') #gpt2 used tanh approximation, dan hendrycks suggested in github comment, nowadays irrelevant
+        self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd)
+
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = self.gelu(x)
+        x = self.c_proj(x)
+        return x
+
+class Block(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        self.ln_1 = nn.LayerNorm(config.n_embd)
+        self.attn = CausalSelfAttention(config)
+        self.ln_2 = nn.LayerNorm(config.n_embd)
+        self.mlp = MLP(config)
+
+    def forward(self, x):
+        x = x + self.attn(self.ln_1(x))
+        x = x + self.mlp(self.ln_2(x))
+        
 
 @dataclass
 class GPTConfig:
@@ -15,13 +67,15 @@ class GPTConfig:
     n_embd: int = 384
 
 class GPT(nn.Module):
+
     def __init__(self, config):
         super().__init__()
         self.config = config
+
         self.transformer = nn.ModuleDict(dict(
-            wte = nn.Embedding(config.vocab_size, config.n_embed),
-            wpe = nn.Embedding(config.block_size, config.n_embd),
-            h = nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
-            ln_f = nn.LayerNorm(config.n_embd),
+            wte = nn.Embedding(config.vocab_size, config.n_embed), # weight token embeddings
+            wpe = nn.Embedding(config.block_size, config.n_embd), # weight positional embeddings
+            h = nn.ModuleList([Block(config) for _ in range(config.n_layer)]), # layers
+            ln_f = nn.LayerNorm(config.n_embd), # final layernorm, introduced by GPT2 paper
         ))
-        self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+        self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False) # final classification head
